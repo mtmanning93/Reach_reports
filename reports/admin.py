9f7b2cc5d5dbd90d4ea92c97e9f4ff1cee47641a
Matt Manning <mtmanning93@hotmail.co.uk>
2023-07-07 16:36:35 +0000

    Create report report admin class

--- reports/admin.py
+++ reports/admin.py
@@ -1,9 +1,20 @@
 from django.contrib import admin
-from .models import Report
+from .models import Report, Comment
 from django_summernote.admin import SummernoteModelAdmin
 
 
 @admin.register(Report)
 class ReportAdmin(SummernoteModelAdmin):
-
+    list_display = (
+        'title',
+        'activity_category',
+        'author',
+        'start_date',
+        'created_on')
+    search_fields = ['title', 'content']
+    prepopulated_fields = {'slug': ('title', 'author', 'created_on',)}
+    list_filter = ('overall_conditions', 'created_on', 'start_date')
     summernote_fields = ('description')
+
+
+
