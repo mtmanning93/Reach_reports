9f98faacf0247b50b629117255adfb9ff70eb3dc
Matt Manning <mtmanning93@hotmail.co.uk>
2023-07-07 16:40:04 +0000

    Create comment admin class and ability to approve comments

--- reports/admin.py
+++ reports/admin.py
@@ -11,10 +11,22 @@ class ReportAdmin(SummernoteModelAdmin):
         'author',
         'start_date',
         'created_on')
-    search_fields = ['title', 'content']
+    search_fields = ['title', 'description']
     prepopulated_fields = {'slug': ('title', 'author', 'created_on',)}
     list_filter = ('overall_conditions', 'created_on', 'start_date')
     summernote_fields = ('description')
 
 
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'content',
+        'report',
+        'created_on')
+    list_filter = ('approved', 'created_on',)
+    search_fields = ['name', 'email', 'content']
+    actions = ['approve_comments']
 
+    def approve_comments(self, request, queryset):
+        queryset.update(approved=True)
