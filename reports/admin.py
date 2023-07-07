910e1473a91a7005eb8b8d5fc9f85a8ff524c4fc
Matt Manning <mtmanning93@hotmail.co.uk>
2023-07-07 16:28:33 +0000

    install summernote and update admin panel to enable use

--- reports/admin.py
+++ reports/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Report
+from django_summernote.admin import SummernoteModelAdmin
 
-# Register your models here.
+
+@admin.register(Report)
+class ReportAdmin(SummernoteModelAdmin):
+
+    summernote_fields = ('description')
