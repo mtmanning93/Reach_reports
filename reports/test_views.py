8c57252f6b8892e0308144a400a3d95471501e18
Matt Manning <mtmanning93@hotmail.co.uk>
2023-07-12 14:44:56 +0000

    Extend landing page tests

--- reports/test_views.py
+++ reports/test_views.py
@@ -1,13 +1,28 @@
 from django.test import TestCase
+from django.urls import reverse
 from .models import Report
 
 
-class TestReportsView(TestCase):
+class TestLandingPage(TestCase):
 
-    def test_get_landing_page(self):
+    def test_get_landing_page_location(self):
         response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
+
+    def test_url_available_by_name(self):
+        response = self.client.get(reverse("home"))
+        self.assertEqual(response.status_code, 200)
+
+    def test_template_name(self):
+        response = self.client.get(reverse("home"))
         self.assertTemplateUsed(response, 'index.html')
+
+    def test_template_content(self):
+        response = self.client.get(reverse("home"))
+        self.assertContains(response, "<h1>Homepage</h1>")
+        self.assertNotContains(response, "Not on the page")
+
+class TestReportsView(TestCase):
 
     def test_report_list(self):
 
