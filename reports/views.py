3c03180a029ecdaec5da21363c17b0b9c5e6c516
Matt Manning <mtmanning93@hotmail.co.uk>
2023-07-14 06:36:29 +0000

    Update template to display username in comment

--- reports/views.py
+++ reports/views.py
@@ -15,21 +15,6 @@ class ReportList(generic.ListView):
     paginate_by = 10
 
 
-# def report_details(request, pk):
-#     report = get_object_or_404(Report, pk=pk)
-#     comments = Comment.objects.filter(report=report).order_by('created_on')
-#     likes_count = report.likes.count()
-#     return render(
-#         request,
-#         'report_details.html',
-#         {
-#             'report': report,
-#             'comments': comments,
-#             'likes_count': likes_count,
-#             'comment_form': CommentForm()
-#         },
-#     )
-
 def report_details(request, pk):
     report = get_object_or_404(Report, pk=pk)
     comments = Comment.objects.filter(report=report).order_by('created_on')
