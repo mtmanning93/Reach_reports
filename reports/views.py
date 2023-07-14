692e8f4b6fb43a163a8291048c8b4b67f41fa87e
Matt Manning <mtmanning93@hotmail.co.uk>
2023-07-14 06:35:25 +0000

    Update reports details view to enable commenting by authenticated users

--- reports/views.py
+++ reports/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
 from django.views import generic
 from .models import Report, Comment
 from .forms import CommentForm
@@ -15,17 +15,44 @@ class ReportList(generic.ListView):
     paginate_by = 10
 
 
+# def report_details(request, pk):
+#     report = get_object_or_404(Report, pk=pk)
+#     comments = Comment.objects.filter(report=report).order_by('created_on')
+#     likes_count = report.likes.count()
+#     return render(
+#         request,
+#         'report_details.html',
+#         {
+#             'report': report,
+#             'comments': comments,
+#             'likes_count': likes_count,
+#             'comment_form': CommentForm()
+#         },
+#     )
+
 def report_details(request, pk):
     report = get_object_or_404(Report, pk=pk)
-    comments = Comment.objects.filter(report=report)
+    comments = Comment.objects.filter(report=report).order_by('created_on')
     likes_count = report.likes.count()
+
+    if request.method == 'POST':
+        comment_form = CommentForm(request.POST)
+        if comment_form.is_valid():
+            comment = comment_form.save(commit=False)
+            comment.report = report
+            comment.save()
+            return redirect('report_details', pk=pk)
+    else:
+        comment_form = CommentForm()
+
     return render(
         request,
         'report_details.html',
         {
             'report': report,
             'comments': comments,
+            'commented': False,
             'likes_count': likes_count,
-            'comment_form': CommentForm()
+            'comment_form': comment_form,
         },
     )
