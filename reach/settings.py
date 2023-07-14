b40d8501557a309da5fbf23519b89ca67b203308
Matt Manning <mtmanning93@hotmail.co.uk>
2023-07-14 08:53:26 +0000

    Update settings for messages

--- reach/settings.py
+++ reach/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 import os
 import sys
 import dj_database_url
+from django.contrib.messages import constants as messages
 if os.path.isfile('env.py'):
     import env
 
@@ -64,6 +65,14 @@ SITE_ID = 1
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
+
+MESSAGE_TAGS = {
+    messages.DEBUG: 'alert-info',
+    messages.INFO: 'alert-info',
+    messages.SUCCESS: 'alert-success',
+    messages.WARNING: 'alert-warning',
+    messages.ERROR: 'alert-danger',
+}
 
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 
