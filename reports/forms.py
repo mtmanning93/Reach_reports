a44623d9f3022f98b5c72a6a0c99707ea22396fd
Matt Manning <mtmanning93@hotmail.co.uk>
2023-07-15 15:52:22 +0000

    Create form for users to create own reports

--- reports/forms.py
+++ reports/forms.py
@@ -1,4 +1,4 @@
-from .models import Comment
+from .models import Report, Comment
 from django import forms
 
 
@@ -6,3 +6,21 @@ class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ('content',)
+
+
+class CreateReportForm(forms.ModelForm):
+    class Meta:
+        model = Report
+        fields = fields = [
+            'title',
+            'slug',
+            'start_date',
+            'end_date',
+            'time_taken',
+            'overall_conditions',
+            'activity_category',
+            'description',
+            'number_in_group',
+            'number_on_route',
+            'gps_map_link'
+        ]
