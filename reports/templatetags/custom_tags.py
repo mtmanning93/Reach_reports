a9474537dd9bb8cc2028dc3ca30372ad2430ba36
Matt Manning <mtmanning93@hotmail.co.uk>
2023-07-25 08:17:49 +0000

    Add image counter to custom tags, update template

--- reports/templatetags/custom_tags.py
+++ reports/templatetags/custom_tags.py
@@ -1,6 +1,7 @@
 from django import template
 from django.contrib.auth.models import User
 from reports.models import Comment
+from reports.models import ImageFile
 
 register = template.Library()
 
@@ -8,3 +9,8 @@ register = template.Library()
 @register.simple_tag
 def count_user_comments(user):
     return Comment.objects.filter(report__author=user).count()
+
+
+@register.simple_tag
+def count_user_images(user):
+    return ImageFile.objects.filter(report__author=user).count()
