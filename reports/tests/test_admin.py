ec4ab20b8f7c8640e9c8725864be349e81065d60
Matt Manning <mtmanning93@hotmail.co.uk>
2023-08-07 11:54:16 +0000

    Update docstrings

--- reports/tests/test_admin.py
+++ reports/tests/test_admin.py
@@ -6,13 +6,21 @@ from reports.admin import CommentAdmin
 
 
 class CommentAdminTest(TestCase):
+    """
+    Unit tests for the CommentAdmin class.
+    """
 
     def setUp(self):
-
+        """
+        Set up the test environment.
+        """
         self.admin = CommentAdmin(Comment, admin.site)
 
     def test_approve_comments(self):
-
+        """
+        Verifies that the approve_comments method sets the 'approved' attribute
+        to True for the chosen comments.
+        """
         queryset = Comment.objects.none()
 
         self.admin.approve_comments(None, queryset)
--- reports/tests/test_backends.py
+++ reports/tests/test_backends.py
@@ -1,15 +1,19 @@
 from django.test import TestCase
-from django.contrib.auth import get_user_model
+from django.contrib.auth.models import User
 from allauth.account.auth_backends import AuthenticationBackend
 
 from reports.backends import EmailAuthenticationBackend
 
-User = get_user_model()
-
 
 class EmailAuthenticationBackendTest(TestCase):
+    """
+    Unit tests for the EmailAuthenticationBackend class.
+    """
 
     def setUp(self):
+        """
+        Set up the test environment.
+        """
 
         self.backend = EmailAuthenticationBackend()
 
@@ -19,6 +23,10 @@ class EmailAuthenticationBackendTest(TestCase):
             password='testpassword')
 
     def test_authenticate_with_valid_credentials(self):
+        """
+        Ensures that a user can be successfully authenticated using valid
+        email and password credentials.
+        """
 
         credentials = {
             'email': 'test@example.com',
@@ -33,7 +41,9 @@ class EmailAuthenticationBackendTest(TestCase):
         self.assertEqual(authenticated_user, self.user)
 
     def test_authenticate_with_invalid_email(self):
-        # Test with invalid email
+        """
+        Tests user cant be authenticated with an invalid email.
+        """
         credentials = {
             'email': 'nonexistent@example.com',
             'password': 'testpassword'
@@ -46,7 +56,9 @@ class EmailAuthenticationBackendTest(TestCase):
         self.assertIsNone(authenticated_user)
 
     def test_authenticate_with_invalid_password(self):
-        # Test invalid password
+        """
+        Tests user cant be authenticated with an invalid password.
+        """
         credentials = {
             'email': 'test@example.com',
             'password': 'wrongpassword'
