b0fe9017323b20d5b0b93bd892189750ea93fcfb
Matt Manning <mtmanning93@hotmail.co.uk>
2023-08-08 11:26:04 +0000

    Add docstrings

--- reports/tests/test_models.py
+++ reports/tests/test_models.py
@@ -5,9 +5,13 @@ from reports.models import Report, Comment
 
 
 class TestModels(TestCase):
-
+    """
+    Unit tests for model behavior and methods.
+    """
     def setUp(self):
-
+        """
+        Set up the test environment before each test method.
+        """
         self.user = User.objects.create(username="testuser")
 
         self.report = Report.objects.create(
@@ -30,23 +34,35 @@ class TestModels(TestCase):
         )
 
     def test_report_status_defaults_to_published(self):
+        """
+        Test that the report status defaults to published.
+        """
         self.assertTrue(self.report.status)
 
     def test_report_string_method_returns_title(self):
+        """
+        Test that the report's string method returns its title.
+        """
         self.assertEqual(str(self.report), self.report.title)
 
     def test_report_number_of_likes(self):
-
+        """
+        Test the calculation of the number of likes for a report object.
+        """
         self.assertEqual(self.report.number_of_likes(), 0)
         self.report.likes.add(self.user)
         self.assertEqual(self.report.number_of_likes(), 1)
 
     def test_comment_approved_defaults_to_true(self):
-
+        """
+        Test that the comment's 'approved' attribute defaults to True.
+        """
         self.assertTrue(self.comment.approved)
 
     def test_comment_string_method_returns_correctly(self):
-
+        """
+        Test that the comment's string method returns the correct format.
+        """
         self.assertEqual(
             str(self.comment),
             f"Comment {self.comment.content} by {self.comment.name}"
